e84a9041dfe7c5f6b44839373d17be36f4dce7c8
Hvitrevs <venusof12pm@proton.me>
2024-02-25 03:21:07 -0500

    run predict

--- app.py
+++ app.py
@@ -4,5 +4,15 @@ from chat import get_response
 
 app = Flask(__name__)
 
+@app.get("/")
 def index_get():
     return render_template("base.html")
+
+@app.post("/predict")
+def predict():
+    text = request.get.json().get("message")
+    # check if the text is valid
+    response = get_response(text)
+    message = {"answer": response}
+    return jsonify(message)
+
